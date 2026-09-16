73beb191b8171a8bc6e03ae53a5bcf872172f6ec
Gahyun Kim <kimssy1121@gmail.com>
2026-09-16 11:08:58 +0900

    26.09.16 - 프로젝트 최초 생성
    - Spring Boot 4.1.1 / Java 21 기반 BE 프로젝트 생성
    - Gradle 기반 프로젝트 구성
    - Spring Web MVC 라이브러리 추가
    - Spring Data JPA 라이브러리 추가
    - MySQL Driver 라이브러리 추가
    - Lombok 라이브러리 추가
    - Spring Boot DevTools 라이브러리 추가
    - 서버 Port 및 Context Path 설정
    - MySQL 데이터베이스 연결 설정
    - JPA 및 Hibernate 설정
    - 애플리케이션 및 DB 연결 정상 구동 확인

---
+++ src/main/java/com/gh/workmind/WorkMindBeApplication.java
@@ -0,0 +1,13 @@
+package com.gh.workmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkMindBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkMindBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gh/workmind/WorkMindBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gh.workmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkMindBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
